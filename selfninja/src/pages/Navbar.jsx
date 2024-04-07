361f52eb6dd064eb8353b557da823c14b9365007
Murali Murugan <murali732000@gmail.com>
2024-04-07 16:48:11 +0100

    add

--- selfninja/src/pages/Navbar.jsx
+++ selfninja/src/pages/Navbar.jsx
@@ -69,9 +69,6 @@ export default function Navbar() {
             </div>
           </div>
 
-
-          
-
           <Disclosure.Panel className="md:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
