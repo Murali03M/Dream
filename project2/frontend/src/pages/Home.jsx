5855f3800c6d13d32f317a32f033a465cf2105e7
Murali Murugan <murali732000@gmail.com>
2024-04-29 12:40:51 +0100

    add the changes in the app.jsx

--- project2/frontend/src/pages/Home.jsx
+++ project2/frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BACKEND_URL } from '../../config';
+import { Spinner } from '../components/spinner';
 
 
 const Home = () => {
   const [userCount, setUserCount] = useState(0);
   const [todoCount, setTodoCount] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,6 +15,10 @@ const Home = () => {
         const response = await axios.get(`${BACKEND_URL}/users/totals`); 
         setUserCount(response.data.userCount);
         setTodoCount(response.data.todoCount);
+        if (response)
+        {
+          setLoading(false)
+          }
       } catch (error) {
         console.error('Error fetching totals:', error);
       }
@@ -24,8 +30,8 @@ const Home = () => {
   return (
     <div className="flex justify-center items-center mt-40 ml-72">
       <div className='flex flex-row items-center text-center'>
-        <h2 className="text-3xl mr-7 bg-slate-400 p-5"> Users total count : {userCount}</h2>
-        <h2 className="text-3xl bg-slate-400 p-5"> Todos total count: {todoCount}</h2>
+        <h2 className="text-3xl mr-7 bg-slate-400 p-5"> Users total count : {loading ? <Spinner/> : userCount}</h2>
+        <h2 className="text-3xl bg-slate-400 p-5"> Todos total count: {loading ? <Spinner/>: todoCount}</h2>
        </div>
       </div>
   );
